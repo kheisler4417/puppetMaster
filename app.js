cee7ffad10501ec3309a1ff3bb9a85480d0b56d4
kheisler4417 <kheisler4417@gmail.com>
2023-04-06 18:26:03 -0400

    express web server

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
