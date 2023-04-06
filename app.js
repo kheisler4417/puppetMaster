516554739c4b5ad200eeb54b3fe3f1df7e156f30
kheisler4417 <kheisler4417@gmail.com>
2023-04-06 18:35:05 -0400

    final app.js updates for initial version

--- app.js
+++ app.js
@@ -12,3 +12,39 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
+
+const puppeteer = require('puppeteer');
+
+let results = [];
+
+app.post('/scrape', async (req, res) => {
+    const { url, selector } = req.body;
+
+    try {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto(url);
+        const data = await page.$$eval(selector, elements => elements.map(e => e.textContent));
+        await browser.close();
+
+        const result = {
+            url,
+            selector,
+            data,
+            timestamp: new Date()
+        };
+        results.unshift(result);
+
+        if (results.length > 10) {
+            results.pop();
+        }
+
+        res.send(result);
+    } catch (error) {
+        res.status(500).send({ error: 'Failed to scrape data' });
+    }
+});
+
+app.get('/past-results', (req, res) => {
+    res.send(results);
+});
